7036b469ef88230f2983788aca24d7ae5d5853f3
Brianwu48 <55972723+Brianwu48@users.noreply.github.com>
2019-10-31 06:30:21 -0400

    Update Person.java

--- classesInheritance/src/classesInheritance/Person.java
+++ classesInheritance/src/classesInheritance/Person.java
@@ -1,34 +1,38 @@
-  
+
 package classesInheritance;
-// ToDo 1: Make this class a child of Person
-public class Student extends Person{
-	double GPA; //GPA class variable in student class
-// ToDo 2: Fix the resulting errors
-	public Student(String name, int age) { 	//Student (Child) constructor with String name and short age parameters linked to parameter variables of Person class (Parent)
-		super(name, age);
-	}
-// ToDo 3: Add a field for GPA and create setter and getter
-	public void setGPA(double T)	//Sets GPA
-		{
-			GPA = T;
-		}
-	public double getGPA()			//Returns GPA
-		{
-			return GPA;
-		}
-	// ToDo 4: Add comments to your code
+
+public abstract class Person {
 	
-	@Override
-	public void setAddress(String address) { //address from parent (this) is assigned address from constructor
-		this.address = address;
-	}
-	@Override
-	public String getAddress() { //gets parent address variable, returns as string
-		return address;
+	protected String name;
+	protected String address;
+	protected int age;
+	
+	// constructor that takes only two paras
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
 	}
 	
-	// ToDo 7: Add a toString method for Student class
-	public String toString() {
-		return "Name: " + name + "\nAge: " + age + "\nGPA: " +  GPA;
+	public String getName() {
+		return name;
 	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public abstract String getAddress() ;
+	
+	public abstract void setAddress(String address);
+	
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
